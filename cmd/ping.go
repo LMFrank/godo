52af86993b6dbcb8c3e2130edb009aa6d4395bc0
LMFrank <njuxushuchi@163.com>
2024-11-05 17:26:24 +0800

    update

--- cmd/ping.go
+++ cmd/ping.go
@@ -5,20 +5,35 @@ import (
 	"github.com/spf13/cobra"
 	"net"
 	"os/exec"
+	"strings"
 )
 
 // pingCmd represents the ping command
 var pingCmd = &cobra.Command{
 	Use:   "ping",
 	Short: "Ping a specified IP or multiple IPs from a YAML file.",
-	Long: `Ping a specified IP address and print the results to the console.
+	Long: `Ping a specified IP address or multiple IPs from a YAML file and print the results to the console or a file.
 For example:
 godo ping 8.8.8.8`,
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		ip := args[0]
-		pingIP(ip)
+		configFile, _ := cmd.Flags().GetString("config")
+		if configFile != "" {
+			pingMultipleIPs(configFile)
+		} else if len(args) > 0 {
+			ip := args[0]
+			pingIP(ip)
+		} else {
+			fmt.Println("Please provide an IP address or a config file")
+		}
 	},
+}
+
+var configFile string
+
+func init() {
+	rootCmd.AddCommand(pingCmd)
+	pingCmd.Flags().StringVarP(&configFile, "config", "c", "", "Path to the YAML config file containing multiple IPs.")
 }
 
 func pingIP(ip string) {
@@ -37,6 +52,27 @@ func pingIP(ip string) {
 	fmt.Printf("Ping results for %s:\n%s", ip, output)
 }
 
-func init() {
-	rootCmd.AddCommand(pingCmd)
+func pingMultipleIPs(configFile string) {
+
+}
+
+func pingSingleIP(ip string) string {
+	if net.ParseIP(ip) == nil {
+		return fmt.Sprintf("Invalid IP address: %s\n", ip)
+	}
+
+	cmd := exec.Command("ping", "-c", "4", ip)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Sprintf("Error pinging %s: %v\n", ip, err)
+	}
+
+	lines := strings.Split(string(output), "\n")
+	var result string
+	for _, line := range lines {
+		if strings.Contains(line, "time=") {
+			result += line + "\n"
+		}
+	}
+	return result
 }
