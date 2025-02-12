e9c32e2fa67fadde8f28461ef117b1819b8927c4
LMFrank <njuxushuchi@163.com>
2025-02-12 17:59:58 +0800

    删除了不需要的execute.go文件

--- util/execute.go
+++
@@ -1,7 +0,0 @@
-package util
-
-// ExecuteCmd 使用 DefaultCommandExecutor 执行命令
-func ExecuteCmd(command string) (err error, result string) {
-	executor := &DefaultCommandExecutor{}
-	return executor.Execute(command)
-}
